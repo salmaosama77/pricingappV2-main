6d74f43d3a45994b40e4d227977777af2706b3a9
Salma Osama <sim.salmaosama3056@alexu.edu.eg>
2023-11-07 18:55:03 +0200

    countryForm_1stCommit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CountryForm from './Components/CountryForm/CountryForm'
+
+function App() {
+  return (
+    <div className="App">
+      <CountryForm/>
+    </div>
+  );
+}
+
+export default App;
